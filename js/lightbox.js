6200776ffbadfccb9a9ae1c3bf19dfc632dbfd2a
Brian Ozawa Burns <BrianOzburns@gmail.com>
2022-10-04 13:28:20 -0700

    gallery sliders

--- js/lightbox.js
+++ js/lightbox.js
@@ -15,6 +15,32 @@ images.forEach(image => {
   });
 });
 
+const artwork = document.querySelectorAll('.art-container');
+artwork.forEach(artpiece => {
+  artpiece.addEventListener('click', e => {
+    lightbox.classList.add('active');
+    const img = document.createElement('img');
+    const image = artpiece.querySelector('img');
+    img.src = image.src;
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild);
+    }
+    lightbox.appendChild(img);
+  });
+});
+
+// for(var i = 0; i < artwork.length; i++){
+//   artwork[i].addEventListener('click', e => {
+//     lightbox.classList.add('active');
+//     const img = document.createElement('img');
+//     img.src = artwork[i].firstChild.src;
+//     while (lightbox.firstChild) {
+//       lightbox.removeChild(lightbox.firstChild);
+//     }
+//     lightbox.appendChild(img);
+//   });
+// }
+
 lightbox.addEventListener('click', e => {
   // if (e.target !== e.currentTarget) return;
   lightbox.classList.remove('active');
